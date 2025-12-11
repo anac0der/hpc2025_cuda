e99061d081300a9c9d8522a7df1dc7929d01e367
Nikita Drozdov <drozdovna@my.msu.ru>
2025-12-11 15:51:50 +0300

    Added sequential version

---
+++ utils.cpp
@@ -0,0 +1,45 @@
+#include "utils.h"
+
+namespace hpc{
+
+Tensor3D::Tensor3D(int n, double hx, double hy, double hz)
+    : dim1(n), dim2(n), dim3(n), data(n * n * n, 0.0), h_x(hx), h_y(hy), h_z(hz) {}
+
+Tensor3D::Tensor3D(int d1, int d2, int d3, double hx, double hy, double hz)
+    : dim1(d1), dim2(d2), dim3(d3), h_x(hx), h_y(hy), h_z(hz), data(d1 * d2 * d3, 0.0) {}
+
+double& Tensor3D::operator()(int i, int j, int k) {
+    return data[(i * dim2 + j) * dim3 + k];
+}
+
+const double& Tensor3D::operator()(int i, int j, int k) const {
+    return data[(i * dim2 + j) * dim3 + k];
+}
+
+
+double laplace_7point(const Tensor3D& u, int i, int j, int k) {
+    double d2u_dx2 = u(i - 1, j, k) - 2.0f * u(i, j, k) + u(i + 1, j, k);
+    double d2u_dy2 = u(i, j - 1, k) - 2.0f * u(i, j, k) + u(i, j + 1, k);
+    double d2u_dz2 = u(i, j, k - 1) - 2.0f * u(i, j, k) + u(i, j, k + 1);
+
+   
+    return d2u_dx2 / std::pow(u.h_x, 2) + d2u_dy2 / std::pow(u.h_y, 2) + d2u_dz2 / std::pow(u.h_z, 2);
+}
+
+double laplace_7point_callable(double (*u)(double, double, double), double x, double y, double z, double h_x, double h_y, double h_z) {
+    double d2u_dx2 = u(x - h_x, y, z) - 2.0f * u(x, y, z) + u(x + h_x, y, z);
+    double d2u_dy2 = u(x, y - h_y, z) - 2.0f * u(x, y, z) + u(x, y + h_y, z);
+    double d2u_dz2 = u(x, y, z - h_z) - 2.0f * u(x, y, z) + u(x, y, z + h_z);
+
+    return d2u_dx2 / std::pow(h_x, 2) + d2u_dy2 / std::pow(h_y, 2) + d2u_dz2 / std::pow(h_z, 2);
+}
+
+double exact_sol(double x, double y, double z, double t){
+    return sin(M_PI * x / L_x) * sin(M_PI * y / L_y) * sin(M_PI * z / L_z) * cos(M_PI * t);
+}
+
+double initial_cond(double x, double y, double z){
+    return exact_sol(x, y, z, 0);
+}
+
+}
---
+++ utils.h
@@ -0,0 +1,36 @@
+#include <vector>
+#include <cmath>
+#include <omp.h>
+
+#define L_x 1.0f
+#define L_y L_x
+#define L_z L_x
+#define A_sq (std::pow(L_x * L_y * L_z, 2) / \
+              (std::pow(L_x, 2) * std::pow(L_y, 2) + \
+               std::pow(L_y, 2) * std::pow(L_z, 2) + \
+               std::pow(L_x, 2) * std::pow(L_z, 2)))
+
+namespace hpc{
+
+class Tensor3D {
+private:
+    std::vector<double> data;
+public:
+    int dim1, dim2, dim3;
+    double h_x, h_y, h_z;
+    Tensor3D(int n, double hx, double hy, double hz);
+    Tensor3D(int d1, int d2, int d3, double hx, double hy, double hz);
+
+    double& operator()(int i, int j, int k);
+    const double& operator()(int i, int j, int k) const;
+};
+
+double laplace_7point(const Tensor3D& u, int i, int j, int k);
+
+double laplace_7point_callable(double (*u)(double, double, double), double x, double y, double z, double h_x, double h_y, double h_z);
+
+double exact_sol(double x, double y, double z, double t);
+
+double initial_cond(double x, double y, double z);
+
+}
